5f52570c586549b411c45503d26f78da3721eb4a
aphsx <aphisit.danc@bumail.net>
2024-09-30 15:56:58 +0700

    Add login form to form-login.php

---
+++ form-login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="" method="POST">
+        <div>
+            <input name="email_account" type="email" placeholder="email" required>
+        </div>
+        <div>
+            <input name="password_account" type="password" placeholder="password" required>
+        </div>
+        <button>Login</button>
+    </form>
+</body>
+</html>
